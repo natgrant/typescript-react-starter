740c636b6491675407afd9cb79c66e2cb0f8b0f4
Nat Grant <email.natgrant@gmail.com>
2019-03-21 20:36:00 +1300

    make component stateful

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -5,17 +5,40 @@ export interface Props {
   enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error("Try being a little more enthusiastic!");
+interface State {
+  currentEnthusiasm: number;
+}
+
+class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
   }
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
+
+  onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+  onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+
+  render() {
+    const { name } = this.props;
+
+    if (this.state.currentEnthusiasm <= 0) {
+      throw new Error("You could be a little more enthusiastic. :D");
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+        </div>
+        <button onClick={this.onDecrement}>-</button>
+        <button onClick={this.onIncrement}>+</button>
       </div>
-    </div>
-  );
+    );
+  }
+
+  updateEnthusiasm(currentEnthusiasm: number) {
+    this.setState({ currentEnthusiasm });
+  }
 }
 
 export default Hello;
