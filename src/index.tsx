cb075433bd8e6f96e5e77bb2a1fd4aa8d9336190
Nat Grant <email.natgrant@gmail.com>
2019-03-22 10:33:31 +1300

    fix dispatch err and store err

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Hello from "./components/HelloStateful";
+
+import Hello from "./containers/Hello";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
+import { StoreState } from "./types/index";
+
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore<StoreState, any, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript"
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById("root") as HTMLElement
 );
-registerServiceWorker();
